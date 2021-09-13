df18fe539696495833c626d164a81f301193dd8a
Luiz Henrique Ferreira <luizhff@gmail.com>
2021-09-13 10:52:03 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'prettier',
+        'plugin:prettier/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:react/jsx-runtime'
+    ],
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    plugins: ['react'],
+    rules: {
+        'no-console': 'off',
+        indent: ['error', 4, { ignoredNodes: ['JSXElement'] }],
+        'class-methods-use-this': 'off',
+        'no-param-reassign': 'off',
+        'comma-dangle': ['error', 'never'],
+        // 'keyword-spacing': [
+        //     'error',
+        //     {
+        //         overrides: {
+        //             if: { after: false }
+        //         }
+        //     }
+        // ],
+        // react
+        'react/jsx-filename-extension': 'off',
+        'react/state-in-constructor': 'off',
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': [2, 4],
+        'react/forbid-prop-types': 'off',
+        'prettier/prettier': 'error',
+        'import/prefer-default-export': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn'
+    }
+};
